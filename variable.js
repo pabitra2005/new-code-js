5e953f5117434ccd237c67a03b47120a42035c05
pabitra <spabitra665@gmail.com>
2025-11-27 15:19:55 +0530

    my variable commit

---
+++ variable.js
@@ -0,0 +1,23 @@
+const accountId =12345
+let accountEmail ="pabitra971@gmail.com"
+var accountPassword = "12345"
+accountcity = "jajpur";
+let accountregion; 
+
+// accountId = 2 NOT ALLOWED
+
+
+// plesed dont use with var beacause it create confusion in code
+
+accountEmail = "spabitra665@gmail.com"
+
+accountcity = "Bengalur"
+
+accountPassword = "124321"
+
+console.log(accountId);
+
+
+
+console.table([accountId,accountEmail,accountcity,accountPassword,accountregion])
+
