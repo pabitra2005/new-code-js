bae21b4d8512cc8df3ca48d42520b6725d65069a
pabitra <spabitra665@gmail.com>
2025-11-27 15:47:33 +0530

    my datatype commit

---
+++ datatype.js
@@ -0,0 +1,30 @@
+"use strict"; 
+// treat all jscode as nnew vwrsion
+
+
+// alert("hello world");
+// we are using node js to run this code
+
+console.log("hello world from console");
+
+console.log(34+45);
+
+
+let firstName = "pabitra";
+
+let age = 22;
+
+let isLoggedIn = false;
+// /string => sequence of characters
+
+// null  =>standalone value
+// symbol => unique value
+
+// objects => key value pairs
+// undifined => value is not assigned
+
+console.table([firstName,age,isLoggedIn]);
+
+console.log(typeof firstName);
+
+console.log(typeof null);
