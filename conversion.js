a4f815c8957a7c6bc81795bbd7a4e1e01526e68d
pabitra <spabitra665@gmail.com>
2025-12-10 16:49:34 +0530

    my conversaion commit

---
+++ conversion.js
@@ -0,0 +1,25 @@
+// let score = true 
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+
+
+// let isLoggedin = 1
+
+// let booleanisLoggedIn = Boolean (isLoggedin)
+
+// console.log(booleanisLoggedIn);
+
+let someNumber =33
+
+let stringNumber =String(someNumber)
+
+console.log( stringNumber);
+console.log(typeof stringNumber);
